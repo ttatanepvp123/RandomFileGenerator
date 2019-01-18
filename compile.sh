f8a86db3e0088a940568dd4a557d216d0cb3981c
Ttatanepvp123 <ttatane974@gmail.com>
2019-01-18 13:24:13 +0100

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 mkdir tmp/
 g++ *.cpp -o tmp/Linux32 -DLAST_COMMIT_ID=\"`git log --format="%H" -n 1`\" --static # For Linux 32bits
-i686-w64-mingw32-g++ *.cpp -o tmp/Win32 -DLAST_COMMIT_ID=\"`git log --format="%H" -n 1`\" --static # For Windows 32bits
-x86_64-w64-mingw32-g++ *.cpp -o tmp/Win64 -DLAST_COMMIT_ID=\"`git log --format="%H" -n 1`\" --static # For Windows 64bits
+i686-w64-mingw32-g++ *.cpp -o tmp/Win32.exe -DLAST_COMMIT_ID=\"`git log --format="%H" -n 1`\" --static # For Windows 32bits
+x86_64-w64-mingw32-g++ *.cpp -o tmp/Win64.exe -DLAST_COMMIT_ID=\"`git log --format="%H" -n 1`\" --static # For Windows 64bits
